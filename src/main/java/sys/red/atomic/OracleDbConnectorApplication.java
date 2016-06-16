61bae0a4a1e4cc7b30aaf3c116072b90a9ca44d1
Andrey <ruan65@gmail.co>
2016-06-16 16:05:25 +0300

    let's get started

---
+++ src/main/java/sys/red/atomic/OracleDbConnectorApplication.java
@@ -0,0 +1,12 @@
+package sys.red.atomic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OracleDbConnectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OracleDbConnectorApplication.class, args);
+	}
+}
---
+++ src/test/java/sys/red/atomic/OracleDbConnectorApplicationTests.java
@@ -0,0 +1,16 @@
+package sys.red.atomic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = OracleDbConnectorApplication.class)
+public class OracleDbConnectorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
